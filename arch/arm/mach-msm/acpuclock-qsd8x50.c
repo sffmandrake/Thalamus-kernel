b2cf24058a130c587f867d0389a5c18c4904f3a2
sffmandrake <sffmandrake@hotmail.fr>
2011-08-05 20:54:53 +0300

    Add 1190mhz

--- arch/arm/mach-msm/acpuclock-qsd8x50.c
+++ arch/arm/mach-msm/acpuclock-qsd8x50.c
@@ -95,9 +95,11 @@ struct clkctl_acpu_speed acpu_freq_tbl[] = {
 //        { 921600, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x18, 0, 1275, 128000 },
 //        { 960000, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x19, 0, 1275, 128000 },
         { 998400, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x1A, 0, 1275, 128000 },
-//	{ 1036800, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1B, 0, 1300, 128000 },
-//	{ 1075200, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1C, 0, 1300, 128000 },
+//	  { 1036800, CCTL(CLK_TCXO, 1),		  SRC_SCPLL, 0x1B, 0, 1300, 128000 },
+//	  { 1075200, CCTL(CLK_TCXO, 1),		  SRC_SCPLL, 0x1C, 0, 1300, 128000 },
 	{ 1113600, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1D, 0, 1300, 128000 },
+//	  { 1152000, CCTL(CLK_TCXO, 1), 	  SRC_SCPLL, 0x1E, 0, 1300, 128000 }, 
+	{ 1190400, CCTL(CLK_TCXO, 1), 		SRC_SCPLL, 0x1F, 0, 1300, 128000 }, 
 	{ 0 },
 };
 
@@ -118,7 +120,7 @@ static struct cpufreq_frequency_table freq_table[ARRAY_SIZE(acpu_freq_tbl)];
 
 static void __init acpuclk_init_cpufreq_table(void)
 {
-	int allowed_speeds[]={245760,384000,576000,768000,998400,1113600};
+	int allowed_speeds[]={245760,384000,576000,768000,998400,1113600,1190400};
 	int i,j;
 
 	for (i = 0; acpu_freq_tbl[i].acpu_khz; i++) {
